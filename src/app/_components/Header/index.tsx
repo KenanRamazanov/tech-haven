6e70ac97fd896751b1eb781187f6d92a54301479
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-06 20:19:15 +0400

    Creating HeaderComponent

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -7,10 +7,7 @@ import Link from 'next/link'
 
 import { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
-import { Gutter } from '../Gutter'
-import { HeaderNav } from './Nav'
-
-import classes from './index.module.scss'
+import {HeaderComponent } from "./HeaderComponent"
 
 export async function Header() {
   let header: Header | null = null
