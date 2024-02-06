209be6aaf7ef3141a574912ff7d3c4ac1a80d18c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-06 20:49:40 +0400

    Final of Header

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,15 +1,29 @@
+'use client'
+
 import React from 'react'
-import { Header } from '../../../../payload/payload-types'
+import Image from 'next/image'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+
 const HeaderComponent = ({ header }: { header: Header }) => {
-    const pathname = usePathname()
+  const pathname = usePathname()
+
   return (
-<>
-<div>
-    salam
-</div>
-</>
+    <nav>
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+        </Link>
+
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
   )
 }
 
