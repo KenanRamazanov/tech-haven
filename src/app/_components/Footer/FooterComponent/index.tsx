adc54ed1fcdf2172444cb7bbaeb68e721ac67c90
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-07 15:49:33 +0400

    Adding images to footer

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,8 +1,36 @@
+'use client'
 import React from 'react'
+import { usePathname } from 'next/navigation'
+
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { Gutter } from '../../Gutter'
 
 import classes from './index.module.scss'
+import Image from 'next/image'
 const FooterComponent = () => {
-  return <div>FooterComponent</div>
+  const pathname = usePathname()
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
+      <Gutter>
+        <ul className={classes.inclusions}>
+        {inclusions.map(inclusion => (
+            <li key={inclusion.title}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.title}
+                width={36}
+                height={36}
+                className={classes.icon}
+              />
+
+              <h5 className={classes.title}>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Gutter>
+    </footer>
+  )
 }
 
 export default FooterComponent
